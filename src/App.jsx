2acb11a9728c866459020a99b04bdc2fae31b831
zaurqurbanov <qurbanovzaur078@gmail.com>
2024-12-10 21:45:08 +0400

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/:id" element={<CardInfo />} />
+        <Route path="qr-info/:id" element={<CardInfo />} />
       </Routes>
     </>
   );
