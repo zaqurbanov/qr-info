3cff6128618c364a91fda7d5158c0e4e71504e82
zaurqurbanov <qurbanovzaur078@gmail.com>
2024-12-14 16:55:27 +0400

    zaur/feat:create new design for card and loading

--- src/pages/CardInfo.jsx
+++ src/pages/CardInfo.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { enumConfig } from "../config/config";
+import { DotLoader } from "react-spinners";
 
 const CardInfo = () => {
   const [userInfoData, setUserInfoData] = useState({});
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const openSocialLink = (url) => {
     const fallbackTimeout = setTimeout(() => {
-      window.open(url, "_blank"); 
+      window.open(url, "_blank");
     }, 1000);
-  
-    window.location.href = url; 
+
+    window.location.href = url;
     clearTimeout(fallbackTimeout);
   };
-  
+
   useEffect(() => {
     const fetchData = async () => {
-      
       try {
         const resposne = await fetch(
           `${enumConfig.BASE_URL}api/v1/qrcode/${id}`
@@ -26,6 +27,7 @@ const CardInfo = () => {
 
         if (data.success) {
           setUserInfoData(data.data);
+          setLoading(false);
         }
       } catch (error) {
         console.log(error);
@@ -35,69 +37,81 @@ const CardInfo = () => {
   }, []);
   console.log(userInfoData);
   return (
-   
-    <div className="flex justify-center items-center   shadow-lg  h-screen  " >
-      <div className="w-full max-w-sm bg-[#00668c] text-[#f5f4f1] font-mono border   border-gray-200 relative  rounded-lg shadow-2xl shadow-slate-700 dark:bg-gray-800 dark:border-gray-700 p-5">
-        <div className="flex flex-col items-center pb-10  ">
-          <img
-            className="w-36 h-36 mb-3 relative mt-[-25%] rounded-full object-cover shadow-lg  p-1  ring-2 ring-gray-300 dark:ring-gray-500"
-            src={`${userInfoData?.fields?.profilePicture}`}
-            alt="User image"
+    <>
+      <div className="flex justify-center  relative items-center  font-sans   h-screen  ">
+        {loading ? (
+          <DotLoader
+            color="#6a5b5b"
+            cssOverride={{}}
+            loading
+            speedMultiplier={1}
           />
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            {`${userInfoData?.fields?.name}  ${userInfoData?.fields?.surname}` }
-          </h5>
-          
-          <span className="text-sm  dark:text-gray-400">
-            {userInfoData?.fields?.workAddress}
-          </span>
-      
-          <span className="text-sm  dark:text-gray-400">
-            {userInfoData?.fields?.workName}
-          </span>
-          
-        </div>
-        <div className="flex flex-col gap-4 p-3">
-          <div className="flex justify-between flex-wrap">
-            <p >Email </p>
-            <p>{userInfoData?.fields?.email}</p>
-          </div>
-          <div className="flex justify-between flex-wrap">
-            <p>Phone</p>
-            <p>{userInfoData?.fields?.phone}</p>
-          </div>
-    {userInfoData?.fields?.local&&(
+        ) : (
+          <div className="max-w-[450px] w-full min-w-60 flex flex-col gap-8 p-5 border">
+            {/* Card header */}
+            <div className="w-full max-w-sm bg-[white]     border-gray-200 relative    dark:bg-gray-800 dark:border-gray-700 ">
+              <div className="flex flex-col items-center pb-10 bg-white rounded-lg customCardShadow ">
+                <img
+                  className="w-36 h-36 mb-3 relative mt-[-25%] rounded-full object-cover shadow-lg  p-1  ring-2 ring-gray-300 dark:ring-gray-500"
+                  src={`${userInfoData?.fields?.profilePicture}`}
+                  alt="User image"
+                />
 
-          <div className="grid grid-cols-2 ">
-            <p>Local adress</p>
-            <p>{userInfoData?.fields?.localAddress}</p>
-          </div>
+                <div className="flex flex-col items-center mt-8">
+                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                    {`${userInfoData?.fields?.name}  ${userInfoData?.fields?.surname}`}
+                  </h5>
 
-    )}
-            <div className="flex  flex-wrap gap-7 justify-center mt-8">
-
-
-          {userInfoData?.fields?.socialNetworks?.map((network, index) => (
-            
-              <div className="flex gap-2 items-center cursor-pointer " 
-              onClick={()=>openSocialLink(network.url)}
-              >
-                <div className="w-10  h-10 min-w-10 min-h-10 rounded-full">
-                  <img
-                    className="rounded-full object-cover shadow-lg  p-1  ring-2 ring-gray-300 dark:ring-gray-500"
-                    src={network.icon}
-                    alt=""
-                  />
+                  <span className="text-sm  dark:text-gray-400">
+                    {userInfoData?.fields?.workAddress}
+                  </span>
                 </div>
-                <p className="capitalize hover:text-gray-500 transition-all " >
-                  {network.name}
+              </div>
+            </div>
+            {/* Card header end */}
+
+            <div className="flex flex-col gap-3">
+              <h2 className="text-gray-400">Information</h2>
+
+              <div className="ps-3">
+                <p className="font-semibold">
+                  Mobile: <span>{userInfoData?.fields?.phone}</span>
+                </p>
+                <p className="font-semibold">
+                  Email: <span>{userInfoData?.fields?.email}</span>
                 </p>
               </div>
-          ))}
             </div>
-        </div>
+            {/* Social networks section */}
+            <div className=" flex flex-col gap-4">
+              <h2 className="text-gray-400">Social profiles</h2>
+
+              <div className="grid grid-cols-4 items-center justify-center ps-3 gap-2">
+                {userInfoData?.fields?.socialNetworks?.map((network, index) => (
+                  <div
+                    className="flex flex-col items-center gap-2 mt-3 "
+                    key={index}
+                  >
+                    <div
+                      className="  justify-center cursor-pointer items-center p-[10px]    md:h-20 md:max-h-20   rounded-md bg-slate-300"
+                      onClick={() => openSocialLink(network.url)}
+                    >
+                      <img
+                        src={network?.icon}
+                        alt=""
+                        className="w-full h-full"
+                      />
+                    </div>
+                    <p className="capitalize text-sm">{network.name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            {/* Social network section end */}
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
